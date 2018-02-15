bade519ec5355070926b04eed266ad1c3f729362
Nedyk <nnyatome@gmail.com>
2018-02-15 15:25:27 -0600

    initial commit

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,25 @@
-app.get("/api/:friends", function(req, res){
+
+var myFriends = require('/friends.js')
+
+app.get("/api/:friends?", function(req, res){
 	var friends = req.params.friends
-	res.send()
+	
+	if(friends){
+		for var i = 0; i<newFriend.length; i++}{
+			if(friends === newFriend[i].routeName){
+				return res.json(newFriend[i]);
+			}
+	}
+	return res.json(false)
+}
+return res.json(newFriend)
 
 	//display JSON of all possible friends 
-})
+});
 
-app.post("/api/friends", function(){
+app.post("/api/friends", function(req, res){
+	var addFriend = req.body;
+	addFriend.routeName = addFriend.name.replace(/\s+/g, "").toLowerCase();
 	//handle incoming survey results 
 	//handle compatibility logic 
 	//user's results will be in an array
