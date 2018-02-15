abe745396b9870e5104354022e39bf4fe00bbb29
Nedyk <nnyatome@gmail.com>
2018-02-15 15:23:54 -0600

    initial commit

---
+++ routing/apiRoutes.js
@@ -0,0 +1,12 @@
+app.get("/api/:friends", function(req, res){
+	var friends = req.params.friends
+	res.send()
+
+	//display JSON of all possible friends 
+})
+
+app.post("/api/friends", function(){
+	//handle incoming survey results 
+	//handle compatibility logic 
+	//user's results will be in an array
+})
