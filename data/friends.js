7cb0008862ebfe85845eada79b4ac098312c0602
Nedyk <nnyatome@gmail.com>
2018-02-16 00:09:17 -0600

    initial commit

--- data/friends.js
+++ data/friends.js
@@ -1,8 +1,19 @@
 var newFriend = [ 
- {    name: 
- 	picture: 
- 	scores: [
- 	]
+ { 
+  "name":"Lisa",
+  "picture":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
      }];
 
 
