c2b8183b30b0776f284625f462af154e725e5e8c
Nedyk <nnyatome@gmail.com>
2018-02-16 22:39:12 -0600

    changes

--- data/friends.js
+++ data/friends.js
@@ -1,20 +1,30 @@
 var newFriend = [ 
- { 
-  "name":"Lisa",
-  "picture":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
-      ]
-     }];
+{
+    "name": "Dina morgans",
+    "picture": "https://www.thesun.co.uk/wp-content/uploads/2016/09/nintchdbpict000003827361.jpg?w=768&strip=all",
+    "scores": [5, 1, 3, 4, 2, 2, 3, 4, 2, 5]
+  },
+  {
+    "name": "Leslie weya",
+    "picture": "http://static.tvtropes.org/pmwiki/pub/images/samwinchesters12_7.jpg",
+    "scores": [3, 2, 4, 1, 1, 2, 3, 5, 3, 3]
+  },
+  {
+    "name": "Wahu choke",
+    "picture": "http://static.tvtropes.org/pmwiki/pub/images/deanwinchesters12_3.jpg",
+    "scores": [2, 2, 2, 5, 4, 3, 1, 3, 4, 4]
+  },
+  {
+    "name": "Tema timmy",
+    "picture": "http://az616578.vo.msecnd.net/files/2016/06/03/636005147130588538-1977197225_kimmy-1.jpg",
+    "scores": [3, 4, 3, 5, 5, 1, 2, 2, 5, 5]
+  },
+  {
+    "name": "Bonde bandy",
+    "picture": "http://i2.lisimg.com/5856402/411full.jpg",
+    "scores": [1, 2, 2, 1, 3, 2, 4, 1, 1, 5]
+  },
+     ];
 
 
    module.exports = newFriend; 
